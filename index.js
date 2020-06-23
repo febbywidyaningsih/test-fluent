f5cf36f7c56e8a699b2688511c3f440b74782f26
danipragustia <danipragustia@disroot.org>
2020-06-23 20:34:02 +0700

    up

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
+const fs = require('fs')
 const path = require('path')
 const ffmpeg = require('fluent-ffmpeg')
 const glob = require('glob')
+const os = require('os');
 
+let output_folder = path.join(__dirname, 'output/')
 let config = require(path.join(__dirname, 'config.json'))
-ffmpeg.setFfmpegPath(path.join(__dirname, 'bin/ffmpeg.exe'))
+
+let ffmpegloc = path.join(__dirname, 'bin/ffmpeg')
+if (os.platform().substring(0,3) == 'win') {
+    ffmpegloc = ffmpegloc + '.exe'
+}
+
+ffmpeg.setFfmpegPath(ffmpegloc)
 
 let current = 0
 let last = 0
 
 function render_video(fname) {
 	return new Promise((resolve,reject)=>{
-		ffmpeg(fname)
-		  .inputOptions('-crf ' + config.crf)
+        ffmpeg(fname)
+          .inputOptions([
+              '-crf ' + config.crf,
+              '-preset ' + config.present
+          ])
 		  .audioCodec('lib' + config.audioCodec)
 		  .videoCodec('lib' + config.videoCodec)
-		  .save(path.basename(fname,path.extname(fname)) + config.prefix + '.mp4')
+		  .save(output_folder + path.basename(fname,path.extname(fname)) + config.prefix + '.mp4')
 		  
 		  .on('start', (commandLine) => {
-			// console.log('Spawned Ffmpeg with command: ' + commandLine
+			console.log('Spawned Ffmpeg with command: ' + commandLine)
 			current++;
 			console.log('['+ current + '/' + last + '] [' + path.basename(fname) + '] - Rendering...')
 		  })
 		  
 		  .on('progress',(progress) => {
 			//console.log(progress)
-			console.log('-> [ Render process on : ' + progress.timemark + ' ]')
+			console.log('-> [ Render process on : ' + progress.timemark + ' ] - ' + progress.currentFps + ' FPS')
 		  })
 		  
 		  .on('end', () => {
-			console.log('Finished one job')
+			console.log('[+] - Finished one job')
 			return resolve()
 		  })
 		  
@@ -47,14 +59,26 @@ getDirectories(__dirname, (err, res) => {
     console.log('Error', err)
   } else {
 	current = 0
-	last = res.length
-	processVideo(res)
+	if (res.length > 0) {
+		last = res.length
+		processVideo(res)
+	} else {
+		console.log('There no videos on input folder.')
+		process.exit()
+	}
   }
 });
 
 async function processVideo(array) {
+	console.log(output_folder)
+	if (fs.existsSync(output_folder) && fs.lstatSync(output_folder).isDirectory()) {
+		// Please wait
+	} else {
+		fs.mkdirSync(path.join(__dirname, 'output'));
+	}
 	for (const item of array) {
 		await render_video(item)
 	}
 	console.log('Done!')
+	process.exit()
 }
