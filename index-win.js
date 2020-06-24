437643831d2f9659c7dd1ae921125e5cf79f1d84
danipragustia <danipragustia@disroot.org>
2020-06-24 20:19:57 +0700

    test

--- index-win.js
+++ index-win.js
@@ -35,17 +35,25 @@ getDirectories(process.cwd(), (err, res) => {
   }
 });
 
-async function processVideo(array) {
-	console.log(output_folder)
-	if (fs.existsSync(output_folder) && fs.lstatSync(output_folder).isDirectory()) {
-		console.log('[+] Target output : ' + output_folder)
+function check_folder(fpath) {
+	if (fs.existsSync(fpath) && fs.lstatSync(fpath).isDirectory()) {
+		console.log('[+] Target output : ' + fpath)
 	} else {
-		console.log('[+] Making folder : ' + output_folder)
-		fs.mkdirSync(path.join(process.cwd(), 'output'));
+		console.log('[+] Making folder : ' + fpath)
+		fs.mkdirSync(fpath);
 	}
+}
+
+async function processVideo(array) {
+	check_folder(output_folder)
 	for (const item of array) {
 		// ffmpeg -crf 23 -preset ultrafast -i D:/Cmder/tastis/remtangan-cli/input/input.mkv -y -filter_complex "subtitles='D\:/Cmder/tastis/remtangan-cli/input/input.mkv'" -acodec libopus -vcodec libx265 D:\Cmder\tastis\remtangan-cli\output\input-uwu.mp4
-		tempy = tempy + ffmpegloc + ' -crf ' + config.crf + ' -preset ' + config.present + ' -i ' + '"' + item + '"' + ' -y -filter_complex "subtitles=' + "'" + item.split(':/').join('\\:/') + "'" + '" ' + '-acodec lib' + config.audioCodec + ' -vcodec lib' + config.videoCodec + ' "' + output_folder + path.basename(item,path.extname(item)) + config.prefix + '.mp4' + '"' + EOL
+		let escaped_path_output = item.split('/').join('\\')
+		escaped_path_output = process.cwd() + '\\output\\' + escaped_path_output.split(process.cwd() + '\\input\\').join('')
+		escaped_path_output = escaped_path_output.split(path.basename(escaped_path_output)).join('')
+		console.log('DEBUG : ' + escaped_path_output)
+		check_folder(escaped_path_output.slice(0, -1))
+		tempy = tempy + ffmpegloc + ' -crf ' + config.crf + ' -preset ' + config.present + ' -i ' + '"' + item + '"' + ' -y -filter_complex "subtitles=' + "'" + item.split(':/').join('\\:/') + "'" + '" ' + '-acodec lib' + config.audioCodec + ' -vcodec lib' + config.videoCodec + ' "' + escaped_path_output + path.basename(item,path.extname(item)) + config.prefix + '.mp4' + '"' + EOL
 	}
 	fs.writeFile('batch.bat', tempy, (err) => {
 		if (err) throw err;
